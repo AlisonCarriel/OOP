92ffa18c037fd99fec1eaaf37f246e0a046c7fbe
AlisonCarriel <alison.carriel.r@gmail.com>
2025-03-07 22:03:46 -0300

    circulo

---
+++ aula05.php
@@ -0,0 +1,22 @@
+<?php
+class Circle {
+    private $radius;
+    
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function getArea() {
+        return round(pi(), 2) * pow($this->radius, 2);
+    }
+
+    public function getCircumference() {
+        return 2 * round(pi(), 2) * $this->radius;
+    }
+}
+
+$circle = new Circle(7);
+echo "Area: " . $circle->getArea() . "</br>";
+echo "Circumference: " . $circle->getCircumference() . "</br>";
+?>
